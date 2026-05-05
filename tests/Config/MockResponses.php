3e03c0667bcea2339407ea8e8c2811b6bb1be2c5
Abdurrahman Tantawy <126220430+AbRahman-ra@users.noreply.github.com>
2026-05-05 12:04:49 +0300

    fix mock response for bulk create

--- tests/Config/MockResponses.php
+++ tests/Config/MockResponses.php
@@ -8,7 +8,9 @@ use Tests\Config\Samples\InvoiceSamples;
 return [
     // invoice
     \HamodaDev\Moyasar\Invoice\APIs\CreateInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE),
-    \HamodaDev\Moyasar\Invoice\APIs\BulkCreateInvoicesRequest::class => MockResponse::make(body: [InvoiceSamples::TEST_INVOICE, InvoiceSamples::TEST_INVOICE_2]),
+    \HamodaDev\Moyasar\Invoice\APIs\BulkCreateInvoicesRequest::class => MockResponse::make(body: [
+        'invoices' => [InvoiceSamples::TEST_INVOICE, InvoiceSamples::TEST_INVOICE_2]
+    ]),
     \HamodaDev\Moyasar\Invoice\APIs\GetInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE_3),
     \HamodaDev\Moyasar\Invoice\APIs\ListInvoicesRequest::class => MockResponse::make(body: [
         'invoices' => [InvoiceSamples::TEST_INVOICE, InvoiceSamples::TEST_INVOICE_2, InvoiceSamples::TEST_INVOICE_3],
@@ -19,3 +21,4 @@ return [
     // payment
     /** TODO */
 ];
+
