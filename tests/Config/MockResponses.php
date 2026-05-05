2bf7b89877c03a34f2c951735b70f861c8a3991e
Abdurrahman Tantawy <126220430+AbRahman-ra@users.noreply.github.com>
2026-05-05 11:02:53 +0300

    importing samples to MockResponses

--- tests/Config/MockResponses.php
+++ tests/Config/MockResponses.php
@@ -1,57 +1,20 @@
 <?php
 
+namespace Tests\Config;
+
 use Saloon\Http\Faking\MockResponse;
-
-const MOCK_URL = 'https://example.com';
-
-const TEST_INVOICE = [
-    'id' => '1234',
-    'status' => 'initiated',
-    'amount' => 1000,
-    'currency' => 'USD',
-    'description' => 'test order',
-    'amount_format' => '10.00 USD',
-    'url' => MOCK_URL,
-    'metadata' => [
-        'order_id' => '1234'
-    ],
-];
-
-const CANCELED_TEST_INVOICE = [
-    ...TEST_INVOICE,
-    'status' => 'canceled',
-];
-
-const TEST_INVOICE_2 = [
-    'id' => '5678',
-    'status' => 'initiated',
-    'amount' => 1000,
-    'currency' => 'SAR',
-    'description' => 'test order 22',
-    'amount_format' => '10.00 SAR',
-    'url' => MOCK_URL,
-];
-
-const TEST_INVOICE_3 = [
-    'id' => '91011',
-    'status' => 'initiated',
-    'amount' => 6900,
-    'currency' => 'EGP',
-    'description' => 'test order #333',
-    'amount_format' => '69.00 EGP',
-    'url' => MOCK_URL,
-];
+use Tests\Config\Samples\InvoiceSamples;
 
 return [
     // invoice
-    \HamodaDev\Moyasar\Invoice\APIs\CreateInvoiceRequest::class => MockResponse::make(body: TEST_INVOICE),
-    \HamodaDev\Moyasar\Invoice\APIs\BulkCreateInvoicesRequest::class => MockResponse::make(body: [TEST_INVOICE, TEST_INVOICE_2]),
-    \HamodaDev\Moyasar\Invoice\APIs\GetInvoiceRequest::class => MockResponse::make(body: TEST_INVOICE_3),
+    \HamodaDev\Moyasar\Invoice\APIs\CreateInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE),
+    \HamodaDev\Moyasar\Invoice\APIs\BulkCreateInvoicesRequest::class => MockResponse::make(body: [InvoiceSamples::TEST_INVOICE, InvoiceSamples::TEST_INVOICE_2]),
+    \HamodaDev\Moyasar\Invoice\APIs\GetInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE_3),
     \HamodaDev\Moyasar\Invoice\APIs\ListInvoicesRequest::class => MockResponse::make(body: [
-        'invoices' => [TEST_INVOICE, TEST_INVOICE_2, TEST_INVOICE_3],
+        'invoices' => [InvoiceSamples::TEST_INVOICE, InvoiceSamples::TEST_INVOICE_2, InvoiceSamples::TEST_INVOICE_3],
     ]),
-    \HamodaDev\Moyasar\Invoice\APIs\UpdateInvoiceRequest::class => MockResponse::make(body: TEST_INVOICE),
-    \HamodaDev\Moyasar\Invoice\APIs\CancelInvoiceRequest::class => MockResponse::make(body: CANCELED_TEST_INVOICE),
+    \HamodaDev\Moyasar\Invoice\APIs\UpdateInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE),
+    \HamodaDev\Moyasar\Invoice\APIs\CancelInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::CANCELED_TEST_INVOICE),
 
     // payment
     /** TODO */
