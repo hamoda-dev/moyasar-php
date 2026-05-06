89da2e42953d7df576d433a307bf935d81a88389
Mohammed Hamoda <hamoda.dev@gmail.com>
2026-05-06 05:12:50 +0200

    refator the MockResponse to be class insted of returned file

--- tests/Config/MockResponses.php
+++ tests/Config/MockResponses.php
@@ -11,18 +11,36 @@ use HamodaDev\Moyasar\Invoice\APIs\GetInvoiceRequest;
 use HamodaDev\Moyasar\Invoice\APIs\ListInvoicesRequest;
 use HamodaDev\Moyasar\Invoice\APIs\UpdateInvoiceRequest;
 
-return [
-    // invoice
-    CreateInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE),
-    BulkCreateInvoicesRequest::class => MockResponse::make(body: [
-        'invoices' => [InvoiceSamples::TEST_INVOICE, InvoiceSamples::TEST_INVOICE_2]
-    ]),
-    GetInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE_3),
-    ListInvoicesRequest::class => MockResponse::make(body: [
-        'invoices' => [InvoiceSamples::TEST_INVOICE, InvoiceSamples::TEST_INVOICE_2, InvoiceSamples::TEST_INVOICE_3],
-    ]),
-    UpdateInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE),
-    CancelInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::CANCELED_TEST_INVOICE),
+class MockResponses
+{
+    public static function getAll(): array
+    {
+        return array_merge(
+            self::invoiceResponses(),
+            // self::paymentResponses(), // TODO: add when payment mocks are ready
+        );
+    }
 
-    //TODO: payment
-];
+    private static function invoiceResponses(): array
+    {
+        return [
+            CreateInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE),
+            BulkCreateInvoicesRequest::class => MockResponse::make(body: [
+                'invoices' => [
+                    InvoiceSamples::TEST_INVOICE,
+                    InvoiceSamples::TEST_INVOICE_2,
+                ],
+            ]),
+            GetInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE_3),
+            ListInvoicesRequest::class => MockResponse::make(body: [
+                'invoices' => [
+                    InvoiceSamples::TEST_INVOICE,
+                    InvoiceSamples::TEST_INVOICE_2,
+                    InvoiceSamples::TEST_INVOICE_3,
+                ],
+            ]),
+            UpdateInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::TEST_INVOICE),
+            CancelInvoiceRequest::class => MockResponse::make(body: InvoiceSamples::CANCELED_TEST_INVOICE),
+        ];
+    }
+}
